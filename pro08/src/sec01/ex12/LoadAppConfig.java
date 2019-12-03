d149934e53c37f260b61ac9263a0538430280622
soulwine-it <skyorwind@gmail.com>
2019-12-03 20:41:26 +0900

    Update load-on-startup

---
+++ pro08/src/sec01/ex12/LoadAppConfig.java
@@ -0,0 +1,48 @@
+package sec01.ex12;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "loadConfig", urlPatterns = { "/loadConfig" }, loadOnStartup = 1)
+public class LoadAppConfig extends HttpServlet {
+	private ServletContext context;
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("LoadAppConfig의 init 메서드 호출");
+		context = config.getServletContext(); //init()메서드에서 ServletContext객체를 얻습니다.
+		
+		String menu_member = context.getInitParameter("menu_member");   //getInitParameter() 메서드로 web.xml의 메뉴 정보를 읽어 옵니다.
+		String menu_order = context.getInitParameter("menu_order");
+		String menu_goods = context.getInitParameter("menu_goods");
+		
+		context.setAttribute("menu_member", menu_member);
+		context.setAttribute("menu_order", menu_order);
+		context.setAttribute("menu_goods", menu_goods);
+		
+	}
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		
+		String menu_member = (String) context.getAttribute("menu_member"); //브라우저 요청 시 ServletContext 객체의 바인딩된 메뉴 항목을 가져 옵니다.
+		String menu_order = (String) context.getAttribute("menu_order");
+		String menu_goods = (String) context.getAttribute("menu_goods");
+		
+		out.print("<html><body>");
+		out.print("<table border=1 cellspacing=0><tr>메뉴 이름</tr>");
+		out.print("<tr><td>" + menu_member + "</td><tr>");
+		out.print("<tr><td>" + menu_order + "</td></tr>");
+		out.print("<tr><td>" + menu_goods + "</td></tr>");
+		out.print("</tr></table></body></html>");
+	}
+}
