c11b92225d1ab686ab489ff03e617133f212d742
soulwine-it <skyorwind@gmail.com>
2019-12-03 17:15:47 +0900

    Update ServletContext binding

---
+++ pro08/src/sec01/ex08/SetServletContext.java
@@ -0,0 +1,30 @@
+package sec01.ex08;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/cset")
+public class SetServletContext extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException{
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		ServletContext context = getServletContext(); //ServletContext 객체를 가져옵니다.
+		List member = new ArrayList();
+		member.add("이순신");
+		member.add(30);;
+		context.setAttribute("member", member);  //ServletContext 객체에 데이터를 바인딩합니다.
+		out.print("<html><body>");
+		out.print("이순신과 30 설정");
+		out.print("</body></html>");
+	}
+}
